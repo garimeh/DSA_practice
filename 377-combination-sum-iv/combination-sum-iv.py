cca055b4eba6d92f0850c90fb105e455c3e7712a
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-09-10 02:14:14 +0530

    Time: 44 ms (74.15%) | Memory: 16.2 MB (97.40%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,18 +1,9 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        if not nums:
-            return 0
-        dp = [-1]*(target+1)
-        def recur(t):
-            if t == 0:
-                return 1
-            if t < 0:
-                return 0
-            if dp[t] != -1:
-                return dp[t]
-            comb = 0
+        dp = [0]*(target+1)
+        dp[0] = 1
+        for t in range(1,target+1):
             for num in nums:
-                comb += recur(t - num)
-            dp[t] = comb
-            return comb
-        return recur(target)
+                if t - num >= 0:
+                    dp[t] += dp[t-num]
+        return dp[target]
