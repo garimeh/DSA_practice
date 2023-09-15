b687022d7b2944fcedc428f90ce337bca1bb31d2
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-09-16 00:27:44 +0530

    Time: 2666 ms (30.19%) | Memory: 77.8 MB (89.13%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,36 +1,24 @@
+import heapq
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         if not points:
             return 0
-        
-        # Manhattan distance between two points
-        def manhattan(p1, p2):
-            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-        
         n = len(points)
-        minCost = [float('inf')] * n
-        visited = [False] * n
-        minCost[0] = 0
-        
-        total_cost = 0
-        for _ in range(n):
-            u = self.getMinCostVertex(minCost, visited)
-            visited[u] = True
-            total_cost += minCost[u]
-            
-            for v in range(n):
-                if not visited[v] and manhattan(points[u], points[v]) < minCost[v]:
-                    minCost[v] = manhattan(points[u], points[v])
-                    
-        return total_cost
+        mincst = 0
+        vis = [0]*n
+        pq = [(0,0)]
 
-    def getMinCostVertex(self, cost, visited):
-        minVal = float('inf')
-        minIndex = -1
-        
-        for v in range(len(cost)):
-            if not visited[v] and cost[v] < minVal:
-                minVal = cost[v]
-                minIndex = v
-                
-        return minIndex
+        def dist(p1,p2):
+            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+        while pq:
+            c,u = heapq.heappop(pq)
+            if vis[u]:
+                continue
+            vis[u] = 1
+            mincst += c
+            for v in range(n):
+                if not vis[v]:
+                    d = dist(points[u], points[v])
+                    heapq.heappush(pq,(d,v))
+        return mincst
