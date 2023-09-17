948154bb4f4be275260bca0688bb61b6fa9a3f65
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-09-18 00:52:54 +0530

    Time: 534 ms (93.46%) | Memory: 17.7 MB (89.27%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,23 +1,22 @@
+import heapq
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        rows, cols = len(heights), len(heights[0])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        dist = [[math.inf for _ in range(cols)] for _ in range(rows)]
+        r, c = len(heights), len(heights[0])
+        dist = [[float("inf") for _ in range(c)] for _ in range(r)]
+        dirx = [(1,0), (0,1), (-1,0), (0,-1)]
         dist[0][0] = 0
-        minHeap = [(0, 0, 0)] 
-        
-        while minHeap:
-            effort, x, y = heappop(minHeap)
+        heap = [(0,0,0)]
 
-            if x == rows - 1 and y == cols - 1:
-                return effort
-            
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                
-                if 0 <= nx < rows and 0 <= ny < cols:
-                    new_effort = max(effort, abs(heights[x][y] - heights[nx][ny]))
-                    
-                    if new_effort < dist[nx][ny]:
-                        dist[nx][ny] = new_effort
-                        heappush(minHeap, (new_effort, nx, ny))
+        while heap:
+            ef, x,y = heappop(heap)
+            if x==r-1 and y == c-1:
+                return ef
+            for dx , dy in dirx:
+                nx , ny = x+dx, y+dy
+
+                if 0<=nx<r and 0<=ny<c :
+                    nef = max(ef, abs(heights[x][y] - heights[nx][ny]))
+
+                    if nef < dist[nx][ny]:
+                        dist[nx][ny] = nef
+                        heappush(heap,(nef, nx,ny))
