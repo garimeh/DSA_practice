31e2844e4ff8bf5f9fd30d3da3cb02ca792c3ecc
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-09-29 00:38:25 +0530

    Time: 409 ms (39.90%) | Memory: 64 MB (9.38%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -2,22 +2,24 @@ class RandomizedSet:
 
     def __init__(self):
         self.randomset = set()
-
+        self.arr = []
     def insert(self, val: int) -> bool:
         if val in self.randomset:
             return False
         self.randomset.add(val)
+        self.arr.append(val)
         return True
 
     def remove(self, val: int) -> bool:
         if val in self.randomset:
             self.randomset.remove(val)
+            self.arr.remove(val)
             return True
         return False
 
     def getRandom(self) -> int:
         ran = random.randint(0, len(self.randomset)-1)
-        return list(self.randomset)[ran]
+        return self.arr[ran]
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
