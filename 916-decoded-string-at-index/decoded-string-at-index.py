37777d7dabb817b05c20c681312db40d58ae7b70
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-09-28 13:55:17 +0530

    Time: 33 ms (86.43%) | Memory: 16.2 MB (85.93%) - LeetSync

---
+++ 916-decoded-string-at-index/decoded-string-at-index.py
@@ -0,0 +1,21 @@
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        decoded_length = 0  
+        for char in s:
+            if char.isdigit():
+
+                decoded_length *= int(char)
+            else:
+                decoded_length += 1
+
+        for i in range(len(s) - 1, -1, -1):
+            current_char = s[i]
+
+            if current_char.isdigit():
+                decoded_length //= int(current_char)
+                k %= decoded_length
+            else:
+                if k == 0 or decoded_length == k:
+                    return current_char  
+                decoded_length -= 1
+        return ""  
