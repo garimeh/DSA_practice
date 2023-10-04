3a82ad0030ee6b5dcf170d38aad8d93659d6bb43
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-10-05 00:17:19 +0530

    Time: 570 ms (16.71%) | Memory: 19.3 MB (90.40%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,55 +1,26 @@
-class ListNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
-
 class MyHashMap:
 
     def __init__(self):
-        self.size = 1000
-        self.table = [None] * self.size
-
-    def _index(self, key: int) -> int:
-        return key % self.size
+        self.keys = []
+        self.values = []
 
     def put(self, key: int, value: int) -> None:
-        idx = self._index(key)
-        if not self.table[idx]:
-            self.table[idx] = ListNode(key, value)
-            return
-        current = self.table[idx]
-        while current:
-            if current.key == key:
-                current.value = value
-                return
-            if not current.next:
-                current.next = ListNode(key, value)
-                return
-            current = current.next
+        if key not in self.keys:
+            self.keys.append(key)
+            self.values.append(value)
+        else:
+            self.values[self.keys.index(key)] = value
 
     def get(self, key: int) -> int:
-        idx = self._index(key)
-        current = self.table[idx]
-        while current:
-            if current.key == key:
-                return current.value
-            current = current.next
-        return -1
+        if key not in self.keys:
+            return -1
+        return self.values[self.keys.index(key)]
 
     def remove(self, key: int) -> None:
-        idx = self._index(key)
-        current = self.table[idx]
-        if not current:
-            return
-        if current.key == key:
-            self.table[idx] = current.next
-            return
-        while current.next:
-            if current.next.key == key:
-                current.next = current.next.next
-                return
-            current = current.next    
+        if key in self.keys:
+            index = self.keys.index(key)
+            self.keys.pop(index)
+            self.values.pop(index)
 
 
 # Your MyHashMap object will be instantiated and called as such:
