249140f659cf58f315488b6fd936cf62d023e89e
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-10-09 13:50:34 +0530

    Time: 42 ms (46.48%) | Memory: 16.2 MB (71.84%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,13 +1,7 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        count = {}
-        result = 0
-        
-        for num in nums:
-            if num in count:
-                result += count[num]
-                count[num] += 1
-            else:
-                count[num] = 1
-        
-        return result
+        res = 0
+        cnt = Counter(nums)
+        for c,f in cnt.items():
+            res += f*(f-1)//2
+        return res
