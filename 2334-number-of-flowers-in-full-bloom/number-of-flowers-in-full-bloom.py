ac4f6c1058f98ad75ec54ac604acbc55050850aa
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-10-12 01:03:12 +0530

    Time: 944 ms (46.67%) | Memory: 43.6 MB (80.00%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.py
@@ -1,19 +1,18 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        flowers.sort()
-        sorted_people = sorted(people)
-        dic = {}
-        heap = []
-        
-        i = 0
-        for person in sorted_people:
-            while i < len(flowers) and flowers[i][0] <= person:
-                heapq.heappush(heap, flowers[i][1])
-                i += 1
-            
-            while heap and heap[0] < person:
-                heapq.heappop(heap)
-            
-            dic[person] = len(heap)
-
-        return [dic[x] for x in people]
+        people = [(p, i) for i,p in enumerate(people)]
+        res = [0]*len(people)
+        cnt = 0
+        start = [f[0] for f in flowers]
+        end = [f[1] for f in flowers]
+        heapq.heapify(start)
+        heapq.heapify(end)
+        for p, i in sorted(people):
+            while start and start[0] <= p:
+                heapq.heappop(start)
+                cnt += 1
+            while end and end[0] < p:
+                heapq.heappop(end)
+                cnt -= 1
+            res[i] = cnt
+        return res
