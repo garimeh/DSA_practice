c5e66a53ffb17c0c8bdebf33cc0c2d7b3d9b3614
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-10-12 15:21:47 +0530

    Time: 47 ms (67.51%) | Memory: 16.6 MB (11.67%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        def help(i,j):
+            if i == 0 and j == 0 and grid[i][j] != 1:
+                return 1
+            if i<0 or j<0: return 0
+            if i>= 0 and j >= 0 and grid[i][j] == 1: 
+                dp[i][j] = 0
+                return 0
+            if dp[i][j] != -1: return dp[i][j]
+            up = help(i-1,j)
+            left = help(i,j-1)
+            dp[i][j] = up+left
+            return up+left
         m, n = len(grid), len(grid[0])
         dp = [[-1]*n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0 and grid[i][j] != 1: dp[i][j] = 1
-                elif i >=0 and j>= 0 and grid[i][j] == 1 : dp[i][j] = 0
-                else: 
-                    up = 0
-                    left = 0
-                    if i>0: up = dp[i-1][j] 
-                    if j>0: left = dp[i][j-1]
-                    dp[i][j] = up+left
-        return dp[m-1][n-1]
+        return help(m-1,n-1)
