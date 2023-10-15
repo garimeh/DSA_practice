8125f30426911f7af65fa192cd379fa60aaca52d
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-10-15 12:05:58 +0530

    Time: 603 ms (15.55%) | Memory: 38.7 MB (47.22%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,21 +1,31 @@
 class Solution:
     def numWays(self, steps: int, arrLen: int) -> int:
-        MOD = 1e9 + 7
+        MOD = int(1e9 + 7)
+        # The farthest index we can reach is min(steps//2, arrLen-1)
+        max_index = min(steps//2, arrLen-1)
+        
+        # Initialize memo with default values
         memo = {}
-        @cache
-        def help(i, s):
-            if i == 0 and s == 0:
-                return 1
-            if s == 0 :
-                return 0
-            if (i,s) in memo:
-                return memo[(i,s)]
-            left, right = 0,0
-            if i-1>=0: 
-                left = help(i-1, s-1)
-            if i+1<arrLen: 
-                right = help(i+1, s-1)
-            stay = help(i, s-1)
-            memo[(i,s)] = (left + right + stay)%MOD
-            return (left + right + stay) % MOD
-        return int(help(0, steps))
+        for i in range(max_index + 1):
+            for s in range(steps + 1):
+                memo[(i, s)] = 0
+        
+        # Base case: when no steps are remaining and at index 0
+        memo[(0, 0)] = 1
+        
+        for s in range(1, steps + 1):
+            for i in range(0, max_index + 1):
+                # Stay
+                memo[(i, s)] = memo[(i, s-1)]
+                
+                # Move left
+                if i-1 >= 0:
+                    memo[(i, s)] += memo[(i-1, s-1)]
+                
+                # Move right
+                if i+1 <= max_index:
+                    memo[(i, s)] += memo[(i+1, s-1)]
+                
+                memo[(i, s)] %= MOD
+    
+        return memo[(0, steps)]
