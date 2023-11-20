9bc65b6e3e66ed690377aa4e512282e2c02fd1cf
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-11-20 10:47:02 +0530

    Time: 725 ms (26.44%) | Memory: 30 MB (15.87%) - LeetSync

---
+++ 790-global-and-local-inversions/global-and-local-inversions.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isIdealPermutation(self, nums: List[int]) -> bool:
+        mx = -1
+        for i in range(len(nums)-2):
+            mx = max(mx, nums[i])
+            if mx > nums[i + 2]:
+                return False
+        return True
