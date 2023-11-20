07a89d6cd69b81c3437884d076820f48fa11226c
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-11-21 00:10:38 +0530

    Time: 811 ms (88.43%) | Memory: 39.3 MB (34.21%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.py
@@ -1,32 +1,11 @@
-from collections import defaultdict
-
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        dst = [0] * len(garbage)
-        for i in range(len(travel)):
-            dst[i + 1] = dst[i] + travel[i]
+        ans = sum(len(g) for g in garbage)
+        G, P, M = False, False, False
+        for i in range(len(travel), 0, -1):
+            G |= 'G' in garbage[i]
+            P |= 'P' in garbage[i]
+            M |= 'M' in garbage[i]
+            ans += travel[i-1] * (G + P + M)
         
-        mp = defaultdict(int)
-        m, p, g = 0, 0, 0
-
-        for i in range(len(garbage)):
-            for c in garbage[i]:
-                mp[c] = i
-                if c == "M":
-                    m += 1
-                elif c == "P":
-                    p += 1
-                else:
-                    g += 1
-
-        ans = 0
-        for c in ["M", "P", "G"]:
-            if c in mp: 
-                ans += dst[mp[c]]
-                if c == "M":
-                    ans += m
-                elif c == "P":
-                    ans += p
-                else:
-                    ans += g
         return ans
