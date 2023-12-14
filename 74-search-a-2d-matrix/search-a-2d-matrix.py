c35410f2bbe5c98e9b39069aed80a4f9d2aa2397
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2023-12-14 07:10:08 +0530

    Time: 49 ms (64.87%) | Memory: 17 MB (12.41%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,21 +1,16 @@
 class Solution:
     def searchMatrix(self, a: List[List[int]], t: int) -> bool:
-
-        def binarys(row):
-            l, h = 0, n-1
-            while l <= h:
-                mid = (l+h)//2
-                if row[mid] == t:
-                    return True
-                elif t > row[mid]:
-                    l = mid+1
-                else:
-                    h = mid - 1
-            return False
-
         m = len(a)
         n = len(a[0])
-        for i in range(m):
-            if a[i][0] <= t <= a[i][n-1]:
-                return binarys(a[i])
+        l, h = 0, m*n - 1
+        while l <= h:
+            mid = (l+h)//2
+            row = mid//n
+            col = mid%n
+            if a[row][col] == t:
+                return True
+            elif t > a[row][col]:
+                l = mid + 1
+            else:
+                h = mid - 1
         return False
