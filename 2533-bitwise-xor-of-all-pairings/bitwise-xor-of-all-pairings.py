a6e1242d80ee59972d2adb7f69a981c0673a9919
Garima Minocha <83217432+garimeh@users.noreply.github.com>
2025-01-17 20:48:05 +0530

    Time: 7 ms (39.88%) | Memory: 36.6 MB (27.17%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.py
@@ -4,10 +4,10 @@ class Solution:
         n, m = len(nums1), len(nums2)
         freq = {}
         for num in nums1:
-            freq[num] = freq.get(num,0) + m
-        for num in nums2:
-            freq[num] = freq.get(num,0) + n
-        for num in freq: 
-            if freq[num]%2:
+            if m%2:
                 ans ^= num
+        for num in nums2:
+            if n%2:
+                ans ^= num
+
         return ans
